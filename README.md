55edeabe06a251957d8e734709d1e483606e237f
Eihwan Shin <syehwany38@gmail.com>
2024-03-15 19:48:11 +0900

    add prisma + init login page

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # character_chatbot_web
 nextJS
+Use Prisma + MongoDB + Next-Auth
+
+### How to install
+```
+npm install --save-dev
+npx prisma generate
+```
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-export default function Main() {
-    return <div>
-        Main Page!
-    </div>;
+import Link from "next/link";
+import styles from "../styles/home.module.css";
+import Image from "next/image";
+export default function Home() {
+    return (
+        <div className={styles.div}>
+            <h1>Character Chatbot</h1>
+            <p>Please log in to continue.</p>
+            <Link href='/login'>
+                <div className={styles.link}>Login</div>
+            </Link>
+            <Link href="https://github.com/shshjhjh4455/character_chatbot_web">
+                <Image src="/github-mark.svg" alt="GitHub Mark" width={48} height={48} />
+            </Link>
+        </div>
+    );
 }
