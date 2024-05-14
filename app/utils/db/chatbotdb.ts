7a2788b68347b77c373c758e111171a66ad5672e
giwon <gseo1277@gmail.com>
2024-05-14 22:00:10 +0900

    .

--- app/utils/db/chatbotdb.ts
+++ app/utils/db/chatbotdb.ts
@@ -1,3 +1,4 @@
+/*
 import { prisma } from "app/utils/prisma";
 
 // Find a chatbot by id
@@ -17,7 +18,27 @@ export async function getChatBots() {
 
     return res;
 }
+*/
+import { prisma } from "app/utils/prisma";
 
-
+// 챗봇 검색 함수
+export async function getChatBots(chatBotId?: string) {
+  try {
+    if (chatBotId) {
+      const res = await prisma.chatBot.findUniqueOrThrow({
+        where: {
+          id: chatBotId,
+        },
+      });
+      return res; 
+    } else {// 챗봇 조회 
+      const res = await prisma.chatBot.findMany();
+      return res; 
+    }
+  } catch (error) {
+    console.error(`Error retrieving ChatBots${chatBotId ? ` with ID ${chatBotId}` : ""}:`, error);
+    throw error; 
+  }
+}
 
 
