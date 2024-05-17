1a9f8b33640f17fd9040265216d13bfa189d6e66
Gukbo <39515692+Gukbo@users.noreply.github.com>
2024-05-17 16:49:26 +0900

    logo+list css

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,20 @@
 import Image from 'next/image'
 import logo from '../public/images/logo.png'
+import { usePathname, useRouter } from 'next/navigation'
+
 
 export default function Logo() {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
     <span>
       <Image
       src = {logo}
       alt="logo"
-      width={65}
-      height={52}
+      width={85}
+      height={62}
       priority
+      onClick={() => router.push("/")}
       />
     </span>
   )
