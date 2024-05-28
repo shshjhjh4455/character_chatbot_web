537234c9251d0c6f6ab1cf0ae492ca7f8dabfa88
Eihwan Shin <syehwany38@gmail.com>
2024-05-29 08:58:26 +0900

    Fix Animation css to responsively

--- components/Animation.js
+++ components/Animation.js
@@ -5,12 +5,19 @@ import lottieJson from "../public/images/lottie.json"
 const DynamicLottie = dynamic(() => import('react-lottie-player'), { ssr: false })
 
 export default function Animation() {
+    const style = {
+        width: '100%',
+        height: '100%',
+        maxWidth: '600px',
+        maxHeight: '400px',
+    }
+
     return (
         <DynamicLottie
             loop
             animationData={lottieJson}
             play
-            style={{ width: 600, height: 400 }}
+            style={style}
         />
     )
 }
--- components/mainPage.tsx
+++ components/mainPage.tsx
@@ -1,20 +1,22 @@
 "use client"
 
 import { usePathname, useRouter } from 'next/navigation'
-import Animation from "../components/Animation"
+import Animation from "components/Animation"
 
 export default function HomePage() {
   const pathname = usePathname();
   const router = useRouter();
   return (
     <section className="text-gray-600 body-font">
-      <div className="flex">
-      </div>
-      <Animation />
       <div className="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
-        <div className="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
+        <div className="w-full flex justify-center mb-10">
+          <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+            <Animation />
+          </div>
+        </div>
+        <div className="w-full lg:w-3/4 xl:w-2/3 flex flex-col mb-16 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">가상 인물 챗봇</h1>
-          <p className="my-8 mb-12 leading-relaxed w-full px-4 sm:px-6">
+          <p className="my-8 mb-12 leading-relaxed text-gray-700">
             영화 속 인물의 성격을 배경, 대사와 행동을 통해 분석하여 <br />
             이를 본따 만든 챗봇과 이야기를 나눌 수 있는 프로젝트입니다.
           </p>
